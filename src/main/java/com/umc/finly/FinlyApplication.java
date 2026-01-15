4b1d495fb76eb8109c0cb7684de7e193b5994d40
yjhss <20211420@sungshin.ac.kr>
2026-01-15 21:20:26 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/umc/finly/FinlyApplication.java
@@ -0,0 +1,13 @@
+package com.umc.finly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlyApplication.class, args);
+	}
+
+}
