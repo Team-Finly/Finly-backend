c77ee70b531f4fa9c7902699ae2c415a8ca27e82
yjhss <20211420@sungshin.ac.kr>
2026-01-21 05:39:02 +0900

    Chore: application.yml 설정 (#1)

--- src/main/java/com/umc/finly/global/config/SwaggerConfig.java
+++ src/main/java/com/umc/finly/global/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI swagger() {
-        Info info = new Info().title("Finly API 명세서").description("UMC 9th Finly 프로젝트의 SpringBoot 서버 API 명세서입니다.").version("0.0.1");
+        Info info = new Info().title("Finly API 명세서").description("Finly의 SpringBoot 서버 API 명세서입니다.").version("0.0.1");
 
         // JWT 토큰 헤더 방식
         String securityScheme = "JWT TOKEN";
