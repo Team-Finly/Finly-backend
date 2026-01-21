d640ad1b172b597d549ba08f1a8f207bbcfd9679
나미 <nayoung@namiui-MacBookAir.local>
2026-01-21 23:12:18 +0900

    Feat: Member entity 생성 (#6)

---
+++ src/main/java/com/umc/finly/domain/member/entity/Member.java
@@ -0,0 +1,63 @@
+package com.umc.finly.domain.member.entity;
+
+import com.umc.finly.global.entity.CreatedDeletedBaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(
+        name = "member",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_member_email", columnNames = "email")
+        }
+)
+public class Member extends CreatedDeletedBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(name = "persona_id")
+    private Long personaId;
+
+    @Column(nullable = false, name = "fin_mind_idx")
+    private Integer finMindIdx;
+
+    @Column(name = "refresh_token", length = 512)
+    private String refreshToken;
+
+    @Column(name = "refresh_token_expired_at")
+    private LocalDateTime refreshTokenExpiredAt;
+
+    @PrePersist
+    protected void onCreate(){
+        if(finMindIdx == null){
+            finMindIdx = 0;
+        }
+    }
+
+    public void updateRefreshToken(String refreshToken, LocalDateTime expiredAt) {
+        this.refreshToken = refreshToken;
+        this.refreshTokenExpiredAt = expiredAt;
+    }
+
+    public void clearRefreshToken() {
+        this.refreshToken = null;
+        this.refreshTokenExpiredAt = null;
+    }
+}
+
