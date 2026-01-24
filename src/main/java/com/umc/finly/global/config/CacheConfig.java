c9102c76aed460a191430ebebaddea61b34acb81
HAN JONGSEO <pantene74@kw.ac.kr>
2026-01-25 05:36:13 +0900

    Feat: 종목 분석 조회 API 현재가 크롤링 구현 (#14)

---
+++ src/main/java/com/umc/finly/global/config/CacheConfig.java
@@ -0,0 +1,32 @@
+package com.umc.finly.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("stockCurrentPrice");
+        /**
+         * 요청 1 -> 네이버 API 호출 -> 캐시 저장
+         * 요청 2 (5초 이내) -> 캐시 반환
+         * 요청 3 (5초 후) -> 다시 네이버 API 호출
+         * 실시간 현재가는 아님 -> 네이버 API 과도 호출 방지 목적, 실시간 비슷하게 유지
+         */
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(5, TimeUnit.SECONDS)
+                        .maximumSize(1000) // 최대 1000개 종목까지만 캐시, 메모리 사용량 통제
+        );
+
+        return cacheManager;
+    }
+}
