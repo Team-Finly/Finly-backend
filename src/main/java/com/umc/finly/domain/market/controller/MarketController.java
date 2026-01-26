3ebcf837f9ec405f7a887b8613ecd89a3e52d338
wonee1 <gimchaewon417@gmail.com>
2026-01-27 02:23:32 +0900

    feat : 마켓 컨트롤러 틀 수정

--- src/main/java/com/umc/finly/domain/market/controller/MarketController.java
+++ src/main/java/com/umc/finly/domain/market/controller/MarketController.java
@@ -16,22 +16,5 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MarketController {
 
-    /**
-     * 코스피 / 코스닥 / 공포·탐욕 지수 조회
-     * - Redis 캐시에 저장된 최신 데이터 반환
-     */
 
-    @GetMapping("/index")
-    public ResponseEntity<ApiResponse<MarketIndexResponse>> getMarketIndex() {
-
-    }
-
-    /**
-     * 실시간 시장 인사이트 조회
-     * - Redis에 집계된 타 유저 데이터 기반
-     */
-    @GetMapping("/insight")
-    public ResponseEntity<ApiResponse<MarketInsightResponse>> getMarketInsight() {
-
-    }
 }
