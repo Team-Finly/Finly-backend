50db0aaaa6ad89314cb7cfdba035b173394a8b47
나미 <nayoung@namiui-MacBookAir.local>
2026-01-26 20:42:49 +0900

    Feat: 페르소나테스트 DTO 생성 (#17)

---
+++ src/main/java/com/umc/finly/domain/member/dto/request/PersonaAnswerReq.java
@@ -0,0 +1,15 @@
+package com.umc.finly.domain.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonaAnswerReq {
+    // 페르소나 질문별 선택 답안 DTO
+
+    private Long questionId;
+    private Long optionId;
+}
