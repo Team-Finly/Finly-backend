6a1f8ec820974df61ac6bf6c9487624f48523a76
leenayoung <nayo@kw.ac.kr>
2026-01-31 17:20:16 +0900

    Refactor: authController에서 엔드포인트에 api 삭제 (#32)

--- src/main/java/com/umc/finly/domain/auth/controller/AuthController.java
+++ src/main/java/com/umc/finly/domain/auth/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
