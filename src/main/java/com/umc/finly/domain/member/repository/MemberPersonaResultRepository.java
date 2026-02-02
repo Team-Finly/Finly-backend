aa284adca2ffd1e2c042e4a5c01f03104a553f09
leenayoung <nayo@kw.ac.kr>
2026-02-02 17:14:23 +0900

    Refactor: Param 패키지 수정 (#39)

--- src/main/java/com/umc/finly/domain/member/repository/MemberPersonaResultRepository.java
+++ src/main/java/com/umc/finly/domain/member/repository/MemberPersonaResultRepository.java
@@ -1,9 +1,9 @@
 package com.umc.finly.domain.member.repository;
 
 import com.umc.finly.domain.member.entity.mapping.MembersPersonasResult;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
