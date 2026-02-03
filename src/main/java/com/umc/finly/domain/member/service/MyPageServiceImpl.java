933b83b14f315bac8706047d2e58dea0c164a209
leenayoung <nayo@kw.ac.kr>
2026-02-03 15:47:36 +0900

    Feat: 내 프로필 조회 서비스 생성 (#59)

--- src/main/java/com/umc/finly/domain/member/service/MyPageServiceImpl.java
+++ src/main/java/com/umc/finly/domain/member/service/MyPageServiceImpl.java
@@ -1,10 +1,12 @@
 package com.umc.finly.domain.member.service;
 
+import com.umc.finly.domain.member.dto.response.MyPageMeRes;
 import com.umc.finly.domain.member.dto.response.MyPagePersonaRes;
+import com.umc.finly.domain.member.entity.Member;
 import com.umc.finly.domain.member.exception.MemberErrorCode;
 import com.umc.finly.domain.member.repository.MemberPersonaResultRepository;
+import com.umc.finly.domain.member.repository.MemberRepository;
 import com.umc.finly.global.apiPayload.exception.CustomException;
-import com.umc.finly.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ public class MyPageServiceImpl implements MyPageService{
     // MyPage 관련 서비스
 
     private final MemberPersonaResultRepository memberPersonaResultRepository;
+    private final MemberRepository memberRepository;
 
     // 내 페르소나 조회
     @Override
@@ -23,5 +26,14 @@ public class MyPageServiceImpl implements MyPageService{
         return memberPersonaResultRepository.findByMemberIdFetchPersona(memberId)
                 .map(MyPagePersonaRes::from)
                 .orElseThrow(()-> new CustomException(MemberErrorCode.PERSONA_RESULT_NOT_FOUND));
+    }
+
+    // 내 프로필 조회
+    @Override
+    public MyPageMeRes getMyInfo(Long memberId){
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(()-> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        return MyPageMeRes.from(member);
     }
 }
