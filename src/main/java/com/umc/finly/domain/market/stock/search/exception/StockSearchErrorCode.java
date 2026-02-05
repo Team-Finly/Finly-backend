b9102f092faecce32790b0e3ebef724289f5c739
yjhss <20211420@sungshin.ac.kr>
2026-02-06 04:33:35 +0900

    Refactor: StockSearchServiceImpl MIN_KEYWORD_LENGTH, 최소 길이 검증 및 에러 처리 로직 삭제 (#104)

--- src/main/java/com/umc/finly/domain/market/stock/search/exception/StockSearchErrorCode.java
+++ src/main/java/com/umc/finly/domain/market/stock/search/exception/StockSearchErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum StockSearchErrorCode implements BaseCode {
+
     EMPTY_KEYWORD(HttpStatus.BAD_REQUEST, "STOCK_SEARCH400", "검색어가 비어 있습니다.");
 
     private final HttpStatus httpStatus;
