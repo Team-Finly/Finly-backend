ecc488b9fbf648bbf472d1fe332150947fa49c09
leenayoung <nayo@kw.ac.kr>
2026-02-06 17:25:34 +0900

    Feat: 비밀번호 변경 DTO 생성 (#118)

---
+++ src/main/java/com/umc/finly/domain/member/dto/request/PasswordChangeReqDTO.java
@@ -0,0 +1,20 @@
+package com.umc.finly.domain.member.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordChangeReqDTO {
+
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    private String newPassword;
+
+    @NotBlank(message = "새 비밀번호 확인은 필수입니다.")
+    private String newPasswordConfirm;
+}
