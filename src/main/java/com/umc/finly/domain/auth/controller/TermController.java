e4bf834b8958606ef4ccec8ce9f65ce50eab4086
leenayoung <nayo@kw.ac.kr>
2026-02-06 17:07:02 +0900

    Feat: 약관 상세 조회 컨트롤러 연결 (#116)

--- src/main/java/com/umc/finly/domain/auth/controller/TermController.java
+++ src/main/java/com/umc/finly/domain/auth/controller/TermController.java
@@ -1,5 +1,6 @@
 package com.umc.finly.domain.auth.controller;
 
+import com.umc.finly.domain.auth.dto.res.TermDetailResDTO;
 import com.umc.finly.domain.auth.dto.res.TermResDTO;
 import com.umc.finly.domain.auth.service.TermQueryService;
 import com.umc.finly.global.apiPayload.response.ApiResponse;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,19 @@ public class TermController {
     @GetMapping
     public ApiResponse<TermResDTO.TermList> getTerms(){
         return ApiResponse.onSuccess(termQueryService.getTerms(), SuccessCode.OK);
+    }
+
+    @Operation(
+            summary = "약관 상세 조회",
+            description = """
+                약관 ID로 약관 상세(제목/본문)를 조회합니다.
+                
+                - 로그인 없이 접근 가능하도록 permitAll 권장
+                - 약관 종류(termType)도 함께 반환합니다.
+                """
+    )
+    @GetMapping("/{termId}")
+    public ApiResponse<TermDetailResDTO> getTermDetail(@PathVariable Long termId){
+        return ApiResponse.onSuccess(termQueryService.getTermDetail(termId), SuccessCode.OK);
     }
 }
