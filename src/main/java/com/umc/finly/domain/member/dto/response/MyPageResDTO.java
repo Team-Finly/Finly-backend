c42c7cecd788d22fd44ae2d206ef2897e31e26da
leenayoung <nayo@kw.ac.kr>
2026-02-07 20:23:02 +0900

    Feat: 마이페이지 조회 DTO 생성 (#138)

---
+++ src/main/java/com/umc/finly/domain/member/dto/response/MyPageResDTO.java
@@ -0,0 +1,34 @@
+package com.umc.finly.domain.member.dto.response;
+
+import com.umc.finly.domain.member.entity.Member;
+import com.umc.finly.domain.member.entity.mapping.MembersPersonasResult;
+import com.umc.finly.domain.member.enums.PersonaUiType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MyPageResDTO {
+    private Long memberId;
+    private String nickname;
+    private PersonaUiType personaType;
+    private Integer finMindIdx;
+    private Long mindPieceCount;
+
+    public static MyPageResDTO of(
+            Member member,
+            MembersPersonasResult result,
+            long mindPieceCount
+    ){
+        return MyPageResDTO.builder()
+                .memberId(member.getId())
+                .nickname(member.getNickname())
+                .personaType(result.getPersona().getPersonaType().toUiType())
+                .finMindIdx(member.getFinMindIdx())
+                .mindPieceCount(mindPieceCount)
+                .build();
+    }
+}
