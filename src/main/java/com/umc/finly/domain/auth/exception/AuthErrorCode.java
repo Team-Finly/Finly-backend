161a14c586b903e477c777d9328f0ef15dd0302f
leenayoung <nayo@kw.ac.kr>
2026-02-11 02:14:40 +0900

    Refactor: AuthErrorCode 양식 통일 (#208)

--- src/main/java/com/umc/finly/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/umc/finly/domain/auth/exception/AuthErrorCode.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuthErrorCode implements BaseCode {
 
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "AUTH409", "이미 가입된 이메일입니다."),
-
     // 400
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "AUTH400_1", "비밀번호 형식이 올바르지 않습니다."),
     INVALID_NICKNAME(HttpStatus.BAD_REQUEST, "AUTH400_2", "닉네임 형식이 올바르지 않습니다."),
@@ -28,13 +26,16 @@ public enum AuthErrorCode implements BaseCode {
     REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "AUTH401_8", "서버에 저장된 refreshToken과 일치하지 않습니다."),
 
     //403
-    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH403", "접근 권한이 없습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH403_1", "접근 권한이 없습니다."),
 
     // 404
-    TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH404", "해당 약관이 없습니다."),
+    TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH404_1", "해당 약관이 없습니다."),
+
+    // 409
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "AUTH409_1", "이미 가입된 이메일입니다."),
 
     // 422
-    REQUIRED_TERM_NOT_AGREED(HttpStatus.UNPROCESSABLE_ENTITY, "AUTH422", "필수 약관에 동의해야 합니다.");
+    REQUIRED_TERM_NOT_AGREED(HttpStatus.UNPROCESSABLE_ENTITY, "AUTH422_1", "필수 약관에 동의해야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
