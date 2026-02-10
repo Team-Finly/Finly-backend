e36abb2abd2db994fd7e52adbe3aaf02a0de738b
leenayoung <nayo@kw.ac.kr>
2026-02-10 23:11:02 +0900

    Refactor: DTO 수정 (#195)

--- src/main/java/com/umc/finly/domain/auth/dto/res/TermResDTO.java
+++ src/main/java/com/umc/finly/domain/auth/dto/res/TermResDTO.java
@@ -1,9 +1,7 @@
 package com.umc.finly.domain.auth.dto.res;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -12,22 +10,18 @@ public class TermResDTO {
 
     // 약관 목록 응답 Wrapper
     @Getter
-    @Builder
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class TermList{
-        private List<TermItem> terms;
+        private final List<TermItem> terms;
     }
 
     // 단일 약관 정보 DTO
     @Getter
-    @Builder
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class TermItem{
-        private Long termId;
-        private String title;       // 화면에 표시할 문구
-        private Boolean required;   // 필수/선택
-        private String type;        // enum
+        private final Long termId;
+        private final String title;       // 화면에 표시할 문구
+        private final Boolean required;   // 필수/선택
+        private final String type;        // enum
     }
 }
