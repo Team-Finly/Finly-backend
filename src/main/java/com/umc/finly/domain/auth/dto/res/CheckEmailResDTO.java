312cdb9eab30a78ec931013ee37a2c70a0db702a
leenayoung <nayo@kw.ac.kr>
2026-02-10 22:38:47 +0900

    Refactor: AUTH DTO 리팩토링 (#191)

--- src/main/java/com/umc/finly/domain/auth/dto/res/CheckEmailResDTO.java
+++ src/main/java/com/umc/finly/domain/auth/dto/res/CheckEmailResDTO.java
@@ -1,20 +1,11 @@
 package com.umc.finly.domain.auth.dto.res;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class CheckEmailResDTO {
 
     private final boolean available;
-
-    // 이메일 중복 확인
-    public static CheckEmailResDTO of(boolean available){
-        return CheckEmailResDTO.builder()
-                .available(available)
-                .build();
-    }
 }
