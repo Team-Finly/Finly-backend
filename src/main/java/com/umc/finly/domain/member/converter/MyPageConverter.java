6d1efb1c556dc89a4523015cb44e2d347b2817ca
leenayoung <nayo@kw.ac.kr>
2026-02-11 00:31:51 +0900

    Refactor: 마이페이지 converter 생성 및 수정 (#201)

---
+++ src/main/java/com/umc/finly/domain/member/converter/MyPageConverter.java
@@ -0,0 +1,53 @@
+package com.umc.finly.domain.member.converter;
+
+import com.umc.finly.domain.member.dto.response.*;
+import com.umc.finly.domain.member.entity.Member;
+import com.umc.finly.domain.member.entity.mapping.MembersPersonasResult;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MyPageConverter {
+
+    public static MyPageResDTO toMyPageResDTO(
+            Member member,
+            MembersPersonasResult result,
+            long mindPieceCount
+    ) {
+        return new MyPageResDTO(
+                member.getId(),
+                member.getNickname(),
+                result.getPersona().getPersonaType().toUiType(),
+                member.getFinMindIdx(),
+                mindPieceCount,
+                member.getProfileImageUrl()
+        );
+    }
+
+    public static MyPagePersonaResDTO toMyPagePersonaResDTO(
+            MembersPersonasResult result
+    ) {
+        return new MyPagePersonaResDTO(
+                result.getPersona().getPersonaType().toUiType(),
+                result.getCreatedAt(),
+                result.getUpdatedAt()
+        );
+    }
+
+    public static MyPageMeResDTO toMyPageMeResDTO(Member member) {
+        return new MyPageMeResDTO(
+                member.getId(),
+                member.getEmail(),
+                member.getNickname(),
+                member.getProfileImageUrl()
+        );
+    }
+
+    public static UpdateNicknameResDTO toUpdateNicknameResDTO(String nickname) {
+        return new UpdateNicknameResDTO(nickname);
+    }
+
+    public static ProfileImageResDTO toProfileImageResDTO(String imageUrl) {
+        return new ProfileImageResDTO(imageUrl);
+    }
+}
