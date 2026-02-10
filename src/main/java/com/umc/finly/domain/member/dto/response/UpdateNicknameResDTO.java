a46973182ed4219b0c71b259dc20ba57781c5fb7
leenayoung <nayo@kw.ac.kr>
2026-02-11 00:29:57 +0900

    Refactor: 마이페이지 DTO 수정 (#201)

--- src/main/java/com/umc/finly/domain/member/dto/response/UpdateNicknameResDTO.java
+++ src/main/java/com/umc/finly/domain/member/dto/response/UpdateNicknameResDTO.java
@@ -1,19 +1,10 @@
 package com.umc.finly.domain.member.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class UpdateNicknameResDTO {
-
     private String nickname;
-
-    public static UpdateNicknameResDTO of(String nickname){
-        return UpdateNicknameResDTO.builder()
-                .nickname(nickname)
-                .build();
-    }
 }
